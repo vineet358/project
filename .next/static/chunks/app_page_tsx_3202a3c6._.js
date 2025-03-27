ebd9c242e39d1839033a0371211c62dc13a41742
NK2552003 <nk2552003@gmail.com>
2025-03-27 19:11:55 +0530

    fixed font family error

---
+++ .next/static/chunks/app_page_tsx_3202a3c6._.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_page_tsx_3202a3c6._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_page_tsx_3202a3c6._.js",
+  "chunks": [
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_e73ec907._.js",
+    "static/chunks/node_modules_62ba82e8._.js",
+    "static/chunks/app_homepage_page_tsx_2aac1c48._.js"
+  ],
+  "source": "dynamic"
+});
---
+++ .next/static/chunks/app_page_tsx_6ffbb84b._.js
@@ -0,0 +1,14 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_page_tsx_6ffbb84b._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_page_tsx_6ffbb84b._.js",
+  "chunks": [
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_2a9ba30d._.js",
+    "static/chunks/node_modules_62ba82e8._.js",
+    "static/chunks/[root of the server]__eccd3321._.js",
+    "static/chunks/[next]_internal_font_google_40eb8069._.css"
+  ],
+  "source": "dynamic"
+});
